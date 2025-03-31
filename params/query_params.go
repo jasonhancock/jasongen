517bd45871b47576284024d9e31b10fc5f1e7300
Jason Hancock <jason@jasonhancock.com>
2025-03-31 10:36:43 -0700

    query params bool and better errors

--- params/query_params.go
+++ params/query_params.go
@@ -2,9 +2,46 @@ package params
 
 import (
 	"fmt"
+	"net/http"
 	"net/url"
 	"strconv"
+
+	"github.com/jasonhancock/jasongen/errors"
 )
+
+type missingQueryParamErr struct {
+	name string
+}
+
+func (e *missingQueryParamErr) Error() string {
+	return fmt.Sprintf("query parameter %q not set", e.name)
+}
+
+func (e *missingQueryParamErr) StatusCode() int {
+	return http.StatusBadRequest
+}
+
+func QueryParamBool(values url.Values, name string, dest *bool, opts ...Option) error {
+	var o options
+	for _, opt := range opts {
+		opt(&o)
+	}
+
+	val, ok := values[name]
+	if o.required && !ok {
+		return &missingQueryParamErr{name}
+	}
+
+	if o.required || (ok && val[0] != "") {
+		boolVal, err := strconv.ParseBool(val[0])
+		if err != nil {
+			return errors.NewHTTP(err, http.StatusBadRequest)
+		}
+		*dest = boolVal
+	}
+
+	return nil
+}
 
 func QueryParamString(values url.Values, name string, dest *string, opts ...Option) error {
 	var o options
@@ -14,8 +51,7 @@ func QueryParamString(values url.Values, name string, dest *string, opts ...Opti
 
 	val, ok := values[name]
 	if o.required && !ok {
-		// TODO: this should be a 400 bad request
-		return fmt.Errorf("query parameter %q not set", name)
+		return &missingQueryParamErr{name}
 	}
 
 	if o.required || (ok && val[0] != "") {
@@ -33,15 +69,13 @@ func QueryParamInt8(values url.Values, name string, dest *int8, opts ...Option) 
 
 	val, ok := values[name]
 	if o.required && !ok {
-		// TODO: this should be a 400 bad request
-		return fmt.Errorf("query parameter %q not set", name)
+		return &missingQueryParamErr{name}
 	}
 
 	if o.required || (ok && val[0] != "") {
 		intVal, err := strconv.ParseInt(val[0], 10, 8)
 		if err != nil {
-			// TODO: this should be a 400 bad request
-			return err
+			return errors.NewHTTP(err, http.StatusBadRequest)
 		}
 		*dest = int8(intVal)
 	}
@@ -57,15 +91,13 @@ func QueryParamInt16(values url.Values, name string, dest *int16, opts ...Option
 
 	val, ok := values[name]
 	if o.required && !ok {
-		// TODO: this should be a 400 bad request
-		return fmt.Errorf("query parameter %q not set", name)
+		return &missingQueryParamErr{name}
 	}
 
 	if o.required || (ok && val[0] != "") {
 		intVal, err := strconv.ParseInt(val[0], 10, 16)
 		if err != nil {
-			// TODO: this should be a 400 bad request
-			return err
+			return errors.NewHTTP(err, http.StatusBadRequest)
 		}
 		*dest = int16(intVal)
 	}
@@ -81,15 +113,13 @@ func QueryParamInt32(values url.Values, name string, dest *int32, opts ...Option
 
 	val, ok := values[name]
 	if o.required && !ok {
-		// TODO: this should be a 400 bad request
-		return fmt.Errorf("query parameter %q not set", name)
+		return &missingQueryParamErr{name}
 	}
 
 	if o.required || (ok && val[0] != "") {
 		intVal, err := strconv.ParseInt(val[0], 10, 32)
 		if err != nil {
-			// TODO: this should be a 400 bad request
-			return err
+			return errors.NewHTTP(err, http.StatusBadRequest)
 		}
 		*dest = int32(intVal)
 	}
@@ -105,15 +135,13 @@ func QueryParamInt64(values url.Values, name string, dest *int64, opts ...Option
 
 	val, ok := values[name]
 	if o.required && !ok {
-		// TODO: this should be a 400 bad request
-		return fmt.Errorf("query parameter %q not set", name)
+		return &missingQueryParamErr{name}
 	}
 
 	if o.required || (ok && val[0] != "") {
 		intVal, err := strconv.ParseInt(val[0], 10, 64)
 		if err != nil {
-			// TODO: this should be a 400 bad request
-			return err
+			return errors.NewHTTP(err, http.StatusBadRequest)
 		}
 		*dest = intVal
 	}
--- params/query_params_test.go
+++ params/query_params_test.go
@@ -62,7 +62,7 @@ func TestQueryParamsString(t *testing.T) {
 	}
 }
 
-func QueryParamsInt32(t *testing.T) {
+func TestQueryParamsInt32(t *testing.T) {
 	tests := []struct {
 		desc     string
 		vals     url.Values
@@ -117,7 +117,7 @@ func QueryParamsInt32(t *testing.T) {
 	}
 }
 
-func QueryParamsInt64(t *testing.T) {
+func TestQueryParamsInt64(t *testing.T) {
 	tests := []struct {
 		desc     string
 		vals     url.Values
@@ -160,6 +160,74 @@ func QueryParamsInt64(t *testing.T) {
 		t.Run(tt.desc, func(t *testing.T) {
 			var dest int64
 			err := QueryParamInt64(tt.vals, "foo", &dest, tt.opts...)
+			if tt.err != nil {
+				require.Error(t, err)
+				require.ErrorContains(t, err, tt.err.Error())
+				return
+			}
+
+			require.NoError(t, err)
+			require.Equal(t, tt.expected, dest)
+		})
+	}
+}
+
+func TestQueryParamsBool(t *testing.T) {
+	tests := []struct {
+		desc     string
+		vals     url.Values
+		expected bool
+		err      error
+		opts     []Option
+	}{
+		{
+			"value set, required=true",
+			url.Values{"foo": []string{"true"}},
+			true,
+			nil,
+			[]Option{Required(true)},
+		},
+		{
+			"value set, required=false",
+			url.Values{"foo": []string{"true"}},
+			true,
+			nil,
+			[]Option{Required(false)},
+		},
+		{
+			"value not set, required=true",
+			url.Values{},
+			false,
+			errors.New(`query parameter "foo" not set`),
+			[]Option{Required(true)},
+		},
+		{
+			"value not set, required=false",
+			url.Values{},
+			false,
+			nil,
+			[]Option{Required(false)},
+		},
+		{
+			"value not a bool, required=true",
+			url.Values{"foo": []string{"blah"}},
+			false,
+			errors.New("invalid syntax"),
+			[]Option{Required(true)},
+		},
+		{
+			"value not a bool, required=false",
+			url.Values{"foo": []string{"blah"}},
+			false,
+			errors.New("invalid syntax"),
+			[]Option{Required(false)},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.desc, func(t *testing.T) {
+			var dest bool
+			err := QueryParamBool(tt.vals, "foo", &dest, tt.opts...)
 			if tt.err != nil {
 				require.Error(t, err)
 				require.ErrorContains(t, err, tt.err.Error())
